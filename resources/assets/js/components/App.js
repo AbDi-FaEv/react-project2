80c2092d22c313ed7ff7c0168e8afe70df34d6b3
Dmitry Abramenko <abramenko@a2design.biz>
2018-02-14 18:52:45 +0600

    bla-bla commit

---
+++ resources/assets/js/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Example from './example/Example'
+
+export default class App extends Component {
+    render() {
+        return (
+            <Example/>
+        );
+    }
+}
---
+++ resources/assets/js/index.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
